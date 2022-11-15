79304d048664c2dc15011453fab9e06ec5d7cca0
kkothari1900 <kareena.bsda@gmail.com>
2022-11-15 09:59:02 -0800

    updating reading in google stock prices and building visualizations

--- main.py
+++ main.py
@@ -17,3 +17,13 @@ startdate = data_frame2
 #Downloading dataset from Google
 data_google = yf.download('GOOG',start = startdate,end = enddate,progress=False)
 
+# When Analyzing stock market, begin with candlestick chart. This chart will analyze the price movements of stock prices.
+data_google["Date"] = data_google.index
+data_google = data_google[["Date","Open","High","Low","Close","Adj Close","Volume"]]
+data_google.reset_index(drop = True,inplace = True)
+print(data_google.head())
+
+# Now we visualize the candelstick chart of the Google Stock prices
+figure1 = go.Figure(data_google =[go.Candlestick(x = data_google["Date"],open = data_google["Open"],high = data_google["High"],low = data_google["Low"],close = data_google["Close"])])
+figure1.update_layout(title = "Google Stock Price Analysis",xaxis_rangeslider_visible = False)
+figure1.show()
